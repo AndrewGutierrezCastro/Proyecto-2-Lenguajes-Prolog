23cc64a2b57de5064c714140b08ea2307f16d2c7
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2020-11-19 22:39:02 -0600

    Machote y tipo de solucion decidido

---
+++ Puente.pl
@@ -0,0 +1,82 @@
+/*
+ * solve_hill_climb(State,History,Moves)
+ *   Moves es la secuencia de movidas requeridas para
+ *   alcanzar un estado final deseado a partir de State.
+ *   History contiene los estados previamente visitados.
+ */
+
+% Si el Estado actual es un estado final, no hay que moverse.
+solve_hill_climb(State,_,[]) :-
+    final_state(State).
+
+/*
+ * Si el Estado actual no es un estado final, genera una movida
+ * para desplazarse a un nuevo estado, y continua la b�squeda a
+ * partir de ese nuevo estado.
+ * Las movidas son intentadas en el orden establecido por la heur�stica
+ * que eval�a la "bondad" de los estados que se alcanzan para cada movida.
+ */
+solve_hill_climb(State,History,[Move|Moves]) :-
+    hill_climb(State,Move),      % generar una nueva Movida en el orden heur�stico
+    update(State,Move,State1),   % calcula nuevo estado usando Movida
+    legal(State1),               % nuevo estado debe ser legal
+    not(member(State1,History)), % debe ser primera vez que se llega al nuevo estado
+    solve_hill_climb(State1,[State1|History],Moves).   % continuar a partir de nuevo estado
+
+/*
+ *  A partir de un Estado devuelve una Movida.
+ *  Primero genera todas las movidas, luego las eval�a usando una heur�stica,
+ *  y finalmente las va usando en orden decreciente de la evaluaci�n obtenida.
+ */
+hill_climb(State,Move) :-
+    findall(M,move(State,M),Moves),         % Encuentra todas las movidas posibles
+    evaluate_and_order(Moves,State,[],MVs), % Eval�a con la heur�stica todas las movidas y las ordena.
+    member((Move,_),MVs).                   % Escoge movidas en orden de heur�stica
+
+
+/*
+ * evaluate_and_order(Movidas,Estado,AcumuladorParcial,MovidasOrdenadas)
+ *   Todas las Movidas del Estado actual
+ *   son evaluadas y almacenadas en orden en MovidasOrdenadas
+ *   Acumulador es donde se van acumulando parcialmente las movidas evaluadas.
+ */
+
+% Caso: procesar la primera movida y continuar recursivamente
+evaluate_and_order([Move|Moves],State,MVs,OrderedMVs) :-
+    update(State,Move,State1),         % obtiene nuevo estado usando movida
+    value(State1,Value),               % calcula el valor heur�sico del nuevo estado
+    insertPair((Move,Value),MVs,MVs1), % inserta en orden el par (movida,valor) en lista de movidas
+    evaluate_and_order(Moves,State,MVs1,OrderedMVs).  % procesa recursivamente el resto de movidas
+    
+% Caso base: no hay m�s movidas que evaluar. Se retorna el acumulador como resultado.
+evaluate_and_order([],_,MVs,MVs).
+
+insertPair(MV,[],[MV]).
+insertPair((M,V),[(M1,V1)|MVs],[(M,V),(M1,V1)|MVs]) :-
+    V >= V1.
+insertPair((M,V),[(M1,V1)|MVs],[(M1,V1)|MVs1]) :-
+    V < V1,insertPair((M,V),MVs,MVs1).
+
+
+/*
+ * Inicializa un problema y lo resuelve.
+ *   Problem: nombre del problema.
+ *   Moves: movidas requeridas para resolver el problema.
+ */
+test_hill_climb(Problem,Moves) :-
+   initial_state(Problem,State),           % obtener un Estado inicial dado Problema
+   solve_hill_climb(State,[State],Moves).  % inicia resoluci�n desde Estado
+
+
+initial_state(zgm,zgm(izq,[zorra,gallina,maiz],[])).
+
+final_state(zgm(der,[],[zorra,gallina,maiz])).
+
+update(zgm(B,I,D),Carga,zgm(B1,I1,D1)).
+    /*:-
+    update_Bote(B,B1),
+    update_margenes(Carga,B,I,D,I1,D1).*/
+value(zgm(_,_,[]),0).
+value(zgm(_,_,[_]),1).
+value(zgm(_,_,[_,_]),2).
+value(zgm(_,_,[_,_,_]),3).
