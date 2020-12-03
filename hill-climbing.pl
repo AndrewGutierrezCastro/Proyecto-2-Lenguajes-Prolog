8754fb7b8510b1216e0db84a09c0e8a2b348b5ec
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2020-12-03 00:48:47 -0600

    Update hill-climbing.pl

--- hill-climbing.pl
+++ hill-climbing.pl
@@ -11,13 +11,13 @@ solve_hill_climb(State,_,[]) :-
 
 /*
  * Si el Estado actual no es un estado final, genera una movida
- * para desplazarse a un nuevo estado, y continua la búsqueda a
+ * para desplazarse a un nuevo estado, y continua la bï¿½squeda a
  * partir de ese nuevo estado.
- * Las movidas son intentadas en el orden establecido por la heurística
- * que evalúa la "bondad" de los estados que se alcanzan para cada movida.
+ * Las movidas son intentadas en el orden establecido por la heurï¿½stica
+ * que evalï¿½a la "bondad" de los estados que se alcanzan para cada movida.
  */
 solve_hill_climb(State,History,[Move|Moves]) :-
-    hill_climb(State,Move),      % generar una nueva Movida en el orden heurístico
+    hill_climb(State,Move),      % generar una nueva Movida en el orden heurï¿½stico
     update(State,Move,State1),   % calcula nuevo estado usando Movida
     legal(State1),               % nuevo estado debe ser legal
     not(member(State1,History)), % debe ser primera vez que se llega al nuevo estado
@@ -25,13 +25,13 @@ solve_hill_climb(State,History,[Move|Moves]) :-
 
 /*
  *  A partir de un Estado devuelve una Movida.
- *  Primero genera todas las movidas, luego las evalúa usando una heurística,
- *  y finalmente las va usando en orden decreciente de la evaluación obtenida.
+ *  Primero genera todas las movidas, luego las evalï¿½a usando una heurï¿½stica,
+ *  y finalmente las va usando en orden decreciente de la evaluaciï¿½n obtenida.
  */
 hill_climb(State,Move) :-
     findall(M,move(State,M),Moves),         % Encuentra todas las movidas posibles
-    evaluate_and_order(Moves,State,[],MVs), % Evalúa con la heurística todas las movidas y las ordena.
-    member((Move,_),MVs).                   % Escoge movidas en orden de heurística
+    evaluate_and_order(Moves,State,[],MVs), % Evalï¿½a con la heurï¿½stica todas las movidas y las ordena.
+    member((Move,_),MVs).                   % Escoge movidas en orden de heurï¿½stica
 
 
 /*
@@ -44,18 +44,18 @@ hill_climb(State,Move) :-
 % Caso: procesar la primera movida y continuar recursivamente
 evaluate_and_order([Move|Moves],State,MVs,OrderedMVs) :-
     update(State,Move,State1),         % obtiene nuevo estado usando movida
-    value(State1,Value),               % calcula el valor heurísico del nuevo estado
+    value(State1,Value),               % calcula el valor heurï¿½sico del nuevo estado
     insertPair((Move,Value),MVs,MVs1), % inserta en orden el par (movida,valor) en lista de movidas
     evaluate_and_order(Moves,State,MVs1,OrderedMVs).  % procesa recursivamente el resto de movidas
     
-% Caso base: no hay más movidas que evaluar. Se retorna el acumulador como resultado.
+% Caso base: no hay mï¿½s movidas que evaluar. Se retorna el acumulador como resultado.
 evaluate_and_order([],_,MVs,MVs).
 
 insertPair(MV,[],[MV]).
 insertPair((M,V),[(M1,V1)|MVs],[(M,V),(M1,V1)|MVs]) :-
-    V >= V1.
+    V =< V1.
 insertPair((M,V),[(M1,V1)|MVs],[(M1,V1)|MVs1]) :-
-    V < V1,insertPair((M,V),MVs,MVs1).
+    V > V1,insertPair((M,V),MVs,MVs1).
 
 
 /*
@@ -65,69 +65,214 @@ insertPair((M,V),[(M1,V1)|MVs],[(M1,V1)|MVs1]) :-
  */
 test_hill_climb(Problem,Moves) :-
    initial_state(Problem,State),           % obtener un Estado inicial dado Problema
-   solve_hill_climb(State,[State],Moves).  % inicia resolución desde Estado
+   solve_hill_climb(State,[State],Moves).  % inicia resoluciï¿½n desde Estado
 
 
 
-% === Relaciones que definen el problema zgm     === %
-% === Son las mismas incluidas en depth-first.pl === %
+   initial_state(puente,estado(izq,[alberto,beatriz,carlos,dora,emilio],[],N)):-
+    tiempo_cruzar(N).
 
-initial_state(zgm,zgm(izq,[zorra,gallina,maiz],[])).
+final_state(estado(der,[],[alberto,beatriz,carlos,dora,emilio],_)).
 
-final_state(zgm(der,[],[zorra,gallina,maiz])).
+value(estado(_,LstDer,LstIzq,N),Value):-
+    personaRapida(P, LstIzq),
+    selectMax(PerLenta, LstIzq),
+    persona(P, ValPerRapid),
+    persona(PerLenta, ValPerLen),
+    length(LstIzq, LargoListaIzq),
+    Value is (ValPerLen - ValPerRapid) + LargoListaIzq.
 
-move(zgm(izq,I,_),Carga):-member(Carga,I).
-move(zgm(der,_,D),Carga):-member(Carga,D).
-move(zgm(_,_,_),solo).
+legal(estado(_,_,_,N)):-
+    N >= 0.
 
-update(zgm(B,I,D),Carga,zgm(B1,I1,D1)):-
-update_Bote(B,B1),
-update_margenes(Carga,B,I,D,I1,D1).
+update(estado(LADO,LstIzq,LstDer,N), Movida, NuevoEstado):-
+    selectMax(PersonaMasLenta,Movida),
+    persona(PersonaMasLenta, ValPerMasLenta),
+    N1 is N - ValPerMasLenta,
+    realizarMovimiento(Movida, estado(LADO,LstIzq,LstDer,N1),ESTADO2), %hace el movimiento 
+    cruzarPuente(ESTADO2, NuevoEstado).
+    
 
-update_Bote(izq,der).
-update_Bote(der,izq).
+move(estado(der,LstIzq,LstDer,N),AMOVER):-     %Devolver en AMOVER la lista de 
+    personaRapida(A, LstDer),
+    AMOVER = [A].        
 
-update_margenes(solo,_,I,D,I,D).
-update_margenes(Carga,izq,I,D,I1,D1):-
-      select(Carga,I,I1),
-      insert(Carga,D,D1).
-update_margenes(Carga,der,I,D,I1,D1):-
-      select(Carga,D,D1),
-      insert(Carga,I,I1).
+move(estado(izq,LstIzq,LstDer,_),AMOVER):-
+    findall(MOVIDAS, selectPersonas(MOVIDAS, LstIzq), SOLREPS),
+    sort(SOLREPS,SOLsinREP),
+    member(AMOVER, SOLsinREP).
+    
+/*
+move(estado(izq,LstIzq,LstDer,_), AMOVER):-     %Devolver en AMOVER la lista de 
+    Si se quiere mover de la izquierda a la derecha
+    se revisa si a la izquierda ya hay una persona rapida
+    para pasar a npersonas lentas y asi reducir el tiempo total
+    LstDer \= [],                                           %debe haber al menos una persona en la lista derecha
+    personaRapida(PersonaIzq, LstIzq),
+    personaRapida(PersonaDer, LstDer),
+    persona(PersonaIzq, PerIzqNum),
+    persona(PersonaDer, PerDerNum),
+    PerDerNum < PerIzqNum,
+    personas_a_la_vez(N),              
+    npersonas_lentas(N ,LstIzq, RESTO, AMOVER). */    %elementos a mover de la lista lstIzq
+                                                    %elementos a mover de la lista lstIzq                                   %elementos a mover de la lista LstDer
 
-insert(X,[Y|Ys],[X,Y|Ys]):-precedes(X,Y).
-insert(X,[Y|Ys],[Y|Zs]):-precedes(Y,X),insert(X,Ys,Zs).
-insert(X,[],[X]).
+/*move(estado(izq,LstIzq,LstDer,_), AMOVER):-
+    length(LstIzq, LstLength),
+    LstLength > 2,
+    select(PersonaRapida, LstIzq, LstIzqNueva),
+    findall(MOVIDAS, selectPersonas(MOVIDAS, LstIzqNueva), SOLREPS),
+    sort(SOLREPS,SOLsinREP),
+    member(AMOVER, SOLsinREP). */
+/*
+move(estado(izq,LstIzq,LstDer,_),AMOVER):-     %Devolver en AMOVER la lista de individuos a cruzar
+    personaRapida(A, LstIzq),                               %
+    npersonas_mas_lentas(LstIzq, RESTO, MOVER),
+    AMOVER = [A|MOVER].  
+*/
 
-select(X,[X|Xs],Xs).
-select(X,[Y|Ys],[Y|Zs]):-select(X,Ys,Zs).
+realizarMovimiento(AMOVER, estado(izq, LstIzq, LstDer, N),NEWESTADO):-
+    selectList(LstIzq,AMOVER,LstIzqRESTO),
+    insertList(AMOVER,LstDer,LstDerRESULTADO),
+    NEWESTADO = estado(izq, LstIzqRESTO, LstDerRESULTADO, N).
 
-% Caso no determinístico
-% precedes(zorra,_).
-% precedes(_,maiz).
+realizarMovimiento(AMOVER, estado(der, LstIzq, LstDer, N),NEWESTADO):-
+    selectList(LstDer,AMOVER,LstDerRESTO),
+    insertList(AMOVER,LstIzq, LstIzqRESULTADO),
+    NEWESTADO = estado(der, LstIzqRESULTADO, LstDerRESTO, N).
 
-% Caso determinístico
-precedes(zorra,gallina).
-precedes(zorra,maiz).
-precedes(gallina,maiz).
+cruzarPuente(estado(izq,LstIzq,LstDer, N), ESTADO2):-
+    ESTADO2 = estado(der, LstIzq, LstDer, N).
+cruzarPuente(estado(der,LstIzq,LstDer, N), ESTADO2):-
+    ESTADO2 = estado(izq, LstIzq, LstDer, N).
+
+persona(alberto, 1).
+persona(beatriz, 2).
+persona(carlos, 5).
+persona(dora, 10).
+persona(emilio, 15).
+
+personas_a_la_vez(3).
+tiempo_cruzar(21).
+listaPersonas(Nombres):-
+    findall(Nombre, persona(Nombre,_),Nombres).
+
+/*Selecciona de la lista las persona mas rapida en cruzarlo*/
+personaRapida(R, [X|XS]):-
+  min(X,Y, R),
+  personaRapida(Y, XS).
+personaRapida(P,[X]):-
+  P = X.
+/*Selecciona la persona con el minimo tiempo de 
+  cruce del puente*/
+min(X,Y,V2):-
+  persona(X,X1),
+  persona(Y,Y1),
+  X1 >= Y1,
+  V2 = Y.
+
+min(X,Y,V2):-
+  persona(X,X1),
+  persona(Y,Y1),
+  X1 < Y1,
+  V2 = X.
+
+max(X,Y,V2):-
+  persona(X,X1),
+  persona(Y,Y1),
+  X1 >= Y1,
+  V2 = X.
+
+max(X,Y,V2):-
+  persona(X,X1),
+  persona(Y,Y1),
+  X1 < Y1,
+  V2 = Y.
 
 
-legal(zgm(izq,_,D)):-not(ilegal(D)).
-legal(zgm(der,I,_)):-not(ilegal(I)).
+/*Selecciona el maximo de una lista de personas*/
+selectMax(P,[X|XS]):-
+  max(X,P1,P),
+  selectMax(P1, XS).
+selectMax(P,[X]):-
+  P = X.
 
-ilegal(L):-member(zorra,L),member(gallina,L).
-ilegal(L):-member(gallina,L),member(maiz,L).
+/*Selecciona las n personas mas lentas de la lista X de perosnas*/
+% npersonas_mas_lentas(2,[alberto,beatriz,carlos,dora,emilio],[],R).
+npersonas_mas_lentas(LISTA,RESTO,RESULTADO):-
+  personas_a_la_vez(N),
+  N1 is N - 1,
+  npersonas_mas_lentas(N1,LISTA,[],RESULTADO),
+  selectList(LISTA,RESULTADO,RESTO).
 
-% === Fin de las relaciones usadas por depth-first.pl ==%
+npersonas_lentas(N,LISTA,RESTO,RESULTADO):-
+  npersonas_mas_lentas(N,LISTA,[],RESULTADO),
+  selectList(LISTA,RESULTADO,RESTO).
+
+npersonas_mas_lentas(0,_,A,A).
+
+npersonas_mas_lentas(_,[],A,A).
+
+npersonas_mas_lentas(N,LISTA,Acumulada,R):-
+  N > 0,
+  selectMax(Persona,LISTA),   %seleccionar la personas con max duracion
+  select(Persona, LISTA, L),  %quitar esa persona de la lista, L es la nueva lista sin el elemento P
+  insert(Persona, Acumulada, Acumulada1), %R1 es la lista R con el valor P insertado
+  N1 is N - 1, 
+  npersonas_mas_lentas(N1, L, Acumulada1, R).
 
 
-% === Relación adicional requerida por hill-climb para resolver zgm. === %
-% === Value/2 es una heurísica que da valores más altos conforme     === %
-% === haya más cosas en la rivera derecha.                           === %
+selectPersonas(AMOVER, LISTA):-
+  personas_a_la_vez(N),
+  selectPersonas(N, LISTA, [], AMOVER).
 
-value(zgm(_,_,[]),0).
-value(zgm(_,_,[_]),1).
-value(zgm(_,_,[_,_]),2).
-value(zgm(_,_,[_,_,_]),3).
+selectPersonas(N, LISTA, Acumulada, Respuesta):-
+  N > 0,
+  N1 is N - 1,
+  member(Persona,LISTA),
+  select(Persona, LISTA, L),  %quitar esa persona de la lista, L es la nueva lista sin el elemento P
+  insert(Persona, Acumulada, Acumulada1), %R1 es la lista R con el valor P insertado
+  selectPersonas(N1, L, Acumulada1, Respuesta).
+
+selectPersonas(0,_,R,R).
+selectPersonas(_,[],R,R).
+/*
+* insert(ElementoInsertado, ListaVieja, ListaNueva)
+*
+* Inserta en orden una de las cosas en una lista.
+* La relaciï¿½n precedes/2 establece el orden de las cosas: z < g < m.
+*/
+insert(X,[Y|Ys],[X,Y|Ys]):-precedes(X,Y).   % Elemento va al inicio
+insert(X,[Y|Ys],[Y|Zs]):-precedes(Y,X),insert(X,Ys,Zs).  % Insertar mï¿½s adentro.
+insert(X,[],[X]).                   % Insertar como ï¿½nico elemento.
+
+
+
+% Caso determinï¿½stico
+precedes(X, Y):- 
+  persona(X, _),
+  persona(Y, _),
+  X@<Y.
+
+/*
+* select(Elemento, ListaQueContieneElemento, ListaSinElemento)
+*
+* Extrae no determisticamente un elemento de una lista que lo contiene
+* y obtiene la lista sin ese elemento.
+*/
+select(X,[X|Xs],Xs).                          % Extrae primer elemento.
+select(X,[Y|Ys],[Y|Zs]):-select(X,Ys,Zs).     % Extrae elemento de mï¿½s adentro.
+
+selectList(LISTA,[X|XS],R):-    %LISTA DONDE SE VAN A EXTRAER
+  select(X, LISTA, RESTO),     %X|XS ES LA LISTA DE ELEMENTOS A EXTRAER
+  selectList(RESTO, XS, R).
+
+selectList(R,[],R).
+
+insertList([A|MOVER], LISTA, RESULTADO):-
+  insert(A, LISTA, RESULTADO1),
+  insertList(MOVER, RESULTADO1, RESULTADO).
+
+insertList([], RESULTADO, RESULTADO).
 
 
